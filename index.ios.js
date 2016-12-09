309c1defdb9fb4c54ff327d006b6b17a3ee842f4
lunarkid <d3dyw4hyud1@gmail.com>
2016-12-09 19:12:25 +0700

    - Remove Eslint - Link react-native-maps

--- index.ios.js
+++ index.ios.js
@@ -11,43 +11,41 @@ import {
   Text,
   View
 } from 'react-native';
+var MapView = require('react-native-maps');
 
 export default class TukangServiceKomputer extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <MapView
+        style={ styles.map }
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+     position: 'absolute',
+     top: 0,
+     left: 0,
+     right: 0,
+     bottom: 0,
+     justifyContent: 'flex-end',
+     alignItems: 'center',
+   },
+   map: {
+     position: 'absolute',
+     top: 0,
+     left: 0,
+     right: 0,
+     bottom: 0,
+   },
 });
 
 AppRegistry.registerComponent('TukangServiceKomputer', () => TukangServiceKomputer);
