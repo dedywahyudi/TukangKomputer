3d57cef7c7ed368ff525c348476de9ea2ec66713
lunarkid <d3dyw4hyud1@gmail.com>
2016-12-14 21:21:20 +0700

    Fix eslint to correct version

--- index.ios.js
+++ index.ios.js
@@ -3,6 +3,6 @@ import { AppRegistry } from 'react-native';
 // @todo remove when RN upstream is fixed
 // console.ignoredYellowBox = ['Warning: Failed propType: SceneView'];
 
-import Example from './components/';
+import Routers from './components/';
 
 AppRegistry.registerComponent('TukangServiceKomputer', () => Routers);
